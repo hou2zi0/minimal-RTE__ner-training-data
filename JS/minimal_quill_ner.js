64c203bf89a5f279e791a5cdb51e9a33db3aefe2
hou2zi0 <max.gruentgens@adwmainz.de>
2019-05-16 10:46:31 +0200

    Further refinement of JS code. Added bulk download. Added usage information and example to README.

--- JS/minimal_quill_ner.js
+++ JS/minimal_quill_ner.js
@@ -1,41 +1,76 @@
 let Inline = Quill.import('blots/inline');
 
-const source = [{
-		"text": "1 :: +a) ANNO / D(OMI)NI M · CCC · L NONASb) · AUGVSTI · O(BIIT) / JOHANN[E]S / [...]RNOS · FILI(US) · D(I)C(T)I · UALME DE SO/B(ER)NH(EIM)c) ·"
-	},
-	{
-		"text": "2 :: +a) ANNO / D(OMI)NI M · CCC · L NONASb) · AUGVSTI · O(BIIT) / JOHANN[E]S / [...]RNOS · FILI(US) · D(I)C(T)I · UALME DE SO/B(ER)NH(EIM)c) ·"
-	},
-	{
-		"text": "3 ::+a) ANNO / D(OMI)NI M · CCC · L NONASb) · AUGVSTI · O(BIIT) / JOHANN[E]S / [...]RNOS · FILI(US) · D(I)C(T)I · UALME DE SO/B(ER)NH(EIM)c) ·"
-	},
-	{
-		"text": "4 :: +a) ANNO / D(OMI)NI M · CCC · L NONASb) · AUGVSTI · O(BIIT) / JOHANN[E]S / [...]RNOS · FILI(US) · D(I)C(T)I · UALME DE SO/B(ER)NH(EIM)c) ·"
-	},
-];
+const BUTTONS = ['PERSON', 'PLACE', 'ORG', 'TITLE'];
 
-//
+let source = [{
+  "text": "Example text."
+}];
+
+// file upload
+const counter = document.getElementById('snippet-counter');
+const total = document.getElementById('snippet-total');
+
+const readFile = function() {
+  d3.json(CONFIG.reader.result, function(data) {
+      return data;
+    })
+    .then(function(data) {
+      source = data;
+      quill.setText(source[0].text);
+      counter.textContent = "1";
+      total.textContent = source.length;
+      return data;
+    }, function(error) {
+      console.log(error);
+      CONFIG.fileLoaded = false;
+    });
+};
+
+const CONFIG = {
+  "fileLoaded": false
+};
+
+const Up = document.getElementById('poly-file')
+  .addEventListener("change", (e) => {
+
+    const filehandle = document.getElementById('poly-file')
+      .files[0];
+    CONFIG.reader = new FileReader();
+
+    if (filehandle) {
+      CONFIG.reader.readAsDataURL(filehandle);
+    }
+
+    CONFIG.reader.addEventListener("load", () => {
+      readFile();
+    }, false);
+  });
+
+const textSnippetCounte = function(cnt, ttl) {
+  const counter = document.getElementById('snippet-counter');
+  counter.value = cnt;
+};
 
 class SPAN extends Inline {
-	static create(value) {
-		let node = super.create();
-		// Sanitize url value if desired
-		node.setAttribute('data-annotation', value[0]);
-		node.setAttribute('data-start', value[1]);
-		node.setAttribute('data-end', value[2]);
-		return node;
-	}
-	static formats(node) {
-		// We will only be called with a node already
-		// determined to be a Link blot, so we do
-		// not need to check ourselves
-		node = {
-			'annotation': node.getAttribute('data-annotation'),
-			'start': node.getAttribute('data-start'),
-			'end': node.getAttribute('data-end')
-		};
-		return node;
-	}
+  static create(value) {
+    let node = super.create();
+    // Sanitize url value if desired
+    node.setAttribute('data-annotation', value[0]);
+    node.setAttribute('data-start', value[1]);
+    node.setAttribute('data-end', value[2]);
+    return node;
+  }
+  static formats(node) {
+    // We will only be called with a node already
+    // determined to be a Link blot, so we do
+    // not need to check ourselves
+    node = {
+      'annotation': node.getAttribute('data-annotation'),
+      'start': node.getAttribute('data-start'),
+      'end': node.getAttribute('data-end')
+    };
+    return node;
+  }
 }
 
 // Registering
@@ -44,150 +79,202 @@ SPAN.tagName = 'span';
 Quill.register(SPAN);
 
 var quill = new Quill('#editor-container', {
-	modules: {
-		toolbar: true
-	},
-	theme: 'snow'
+  modules: {
+    toolbar: true
+  },
+  theme: 'snow'
 });
 
 quill.setText(source[0].text);
 
-['PERSON', 'PLACE', 'ORG', 'TITLE'].forEach((item) => {
-	document.getElementById(`${item.toLowerCase()}-button`)
-		.addEventListener('click', function () {
-			if (quill.getFormat()
-				.SPAN === undefined) {
-				console.log('undef');
-				let {
-					index,
-					length
-				} = quill.getSelection();
-				quill.format('SPAN', [item, index, index + length]);
-			} else if (quill.getFormat()
-				.SPAN.annotation === item) {
-				let {
-					index,
-					length
-				} = quill.getSelection();
-				quill.removeFormat(index, length);
-			} else {
-				let {
-					index,
-					length
-				} = quill.getSelection();
-				quill.format('SPAN', [item, index, index + length]);
-			}
-		});
+BUTTONS.forEach((item) => {
+  document.getElementById(`${item.toLowerCase()}-button`)
+    .addEventListener('click', function() {
+      if (quill.getFormat()
+        .SPAN === undefined) {
+        console.log('undef');
+        let {
+          index,
+          length
+        } = quill.getSelection();
+        quill.format('SPAN', [item, index, index + length]);
+      } else if (quill.getFormat()
+        .SPAN.annotation === item) {
+        let {
+          index,
+          length
+        } = quill.getSelection();
+        quill.removeFormat(index, length);
+      } else {
+        let {
+          index,
+          length
+        } = quill.getSelection();
+        quill.format('SPAN', [item, index, index + length]);
+      }
+    });
 });
 
 
 // Download
-const prepareDownload = function (data, filename) {
-	const content = JSON.stringify(data)
-		.replace(/\[null\]/g, '[[0,0]]');
-	const element = document.createElement('a');
-	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
-	element.setAttribute('download', filename);
-	element.style.display = 'none';
-	document.body.appendChild(element);
-	element.click();
-	document.body.removeChild(element);
+const prepareDownload = function(data, filename) {
+  const content = JSON.stringify(data)
+    .replace(/\[null\]/g, '[[0,0]]');
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 };
 
 document.getElementById('download-button')
-	.addEventListener('click', function () {
-		console.log(quill.getContents());
-		let editor = document.getElementById('editor-container')
-			.getElementsByClassName('ql-editor')[0].innerHTML;
-		console.log(`<doc>${editor}</doc>`);
-		console.log(quill.getText());
-		console.log(quill.getContents());
-		let text = quill.getText();
-		// {"entities": [(15, 20, "GPE"), (24, 32, "GPE")]},
-		let entities = [];
-		quill.getContents()
-			.ops.forEach((item) => {
-				if (item.hasOwnProperty('attributes')) {
-					entities.push([item.attributes.SPAN.start, item.attributes.SPAN.end, item.attributes.SPAN.annotation]);
-				}
-			});
-		let out = {
-			"text": text,
-			"entities": entities
-		};
-		console.log(JSON.stringify(out));
-		out.entities.forEach((item) => {
-			let x = out.text.slice(item[0], item[1]);
-			console.log(`<${x}> (${item[2]})`);
-		});
-		prepareDownload(out, "bing.json");
-	});
+  .addEventListener('click', function() {
+    let editor = document.getElementById('editor-container')
+      .getElementsByClassName('ql-editor')[0].innerHTML;
+    let text = quill.getText();
+    // {"entities": [(15, 20, "GPE"), (24, 32, "GPE")]},
+    let entities = [];
+    quill.getContents()
+      .ops.forEach((item) => {
+        if (item.hasOwnProperty('attributes')) {
+          entities.push([item.attributes.SPAN.start, item.attributes.SPAN.end, item.attributes.SPAN.annotation]);
+        }
+      });
+    let out = {
+      "text": text,
+      "entities": entities
+    };
+    console.log(JSON.stringify(out));
+    out.entities.forEach((item) => {
+      let x = out.text.slice(item[0], item[1]);
+      console.log(`<${x}> (${item[2]})`);
+    });
+    prepareDownload(out, "bing.json");
+  });
+
+//Bulk download
+document.getElementById('bulk-download-button')
+  .addEventListener('click', function() {
+    // Set properties of active text
+    const active_item = parseInt(document.getElementById('snippet-counter')
+      .textContent);
+    console.log(active_item);
+    source[active_item - 1].annotations = quill.getContents()
+      .ops;
+
+    // Build object array for bulk download
+    const bulk = [];
+
+    source.forEach((item) => {
+      let text = item.text;
+      let entities = [];
+      console.log(item);
+      try {
+        item.annotations
+          .forEach((item) => {
+            if (item.hasOwnProperty('attributes')) {
+              entities.push([item.attributes.SPAN.start, item.attributes.SPAN.end, item.attributes.SPAN.annotation]);
+            }
+          });
+      } catch (e) {
+        console.log(e);
+      }
+      let out = {
+        "text": text,
+        "entities": entities
+      };
+      bulk.push(out);
+    });
+
+    prepareDownload(bulk, "bulk.json");
+  });
 
 // Prev next buttons
 let current = 0;
 
+// ToDo: Simplify
+const previous = function() {
+  current -= 1;
+  if (current >= 0) {
+    console.log(current);
+    console.log(source.length);
+    source[current + 1].annotations = quill.getContents()
+      .ops;
+    source[current + 1].delta = document.getElementById('editor-container')
+      .getElementsByClassName('ql-editor')[0].innerHTML;
+    console.log(source[current + 1].delta);
+    counter.textContent = current + 1;
+    if (source[current].delta) {
+      document.getElementById('editor-container')
+        .getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
+    } else {
+      quill.setText(source[current].text);
+    }
+  } else {
+    console.log('end of array');
+    current = 0;
+  }
+};
+
 document.getElementById('pre-button')
-	.addEventListener('click', function () {
-		current -= 1;
-		if (current >= 0) {
-			console.log(current);
-			console.log(source.length);
-			source[current + 1].delta = document.getElementById('editor-container')
-				.getElementsByClassName('ql-editor')[0].innerHTML;
-			console.log(source[current + 1].delta);
-			if (source[current].delta) {
-				document.getElementById('editor-container')
-					.getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
-			} else {
-				quill.setText(source[current].text);
-			}
-		} else {
-			console.log('end of array');
-			current = 0;
-		}
-	});
+  .addEventListener('click', function() {
+    previous();
+  });
+
+const next = function() {
+  current += 1;
+  if (current < source.length) {
+    console.log(current);
+    console.log(source.length);
+    source[current - 1].annotations = quill.getContents()
+      .ops;
+    source[current - 1].delta = document.getElementById('editor-container')
+      .getElementsByClassName('ql-editor')[0].innerHTML;
+    console.log(source[current - 1].delta);
+    counter.textContent = current + 1;
+    if (source[current].delta) {
+      document.getElementById('editor-container')
+        .getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
+    } else {
+      quill.setText(source[current].text);
+    }
+  } else {
+    console.log('end of array');
+    current = source.length - 1;
+  }
+};
 
 document.getElementById('next-button')
-	.addEventListener('click', function () {
-		current += 1;
-		if (current < source.length) {
-			console.log(current);
-			console.log(source.length);
-			source[current - 1].delta = document.getElementById('editor-container')
-				.getElementsByClassName('ql-editor')[0].innerHTML;
-			console.log(source[current - 1].delta);
-			if (source[current].delta) {
-				document.getElementById('editor-container')
-					.getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
-			} else {
-				quill.setText(source[current].text);
-			}
-		} else {
-			console.log('end of array');
-			current = source.length - 1;
-		}
-	});
+  .addEventListener('click', function() {
+    next();
+  });
 
 document.getElementById('editor-container')
-	.addEventListener('keydown', function (event) {
-		event.stopPropagation();
-	});
+  .addEventListener('keydown', function(event) {
+    event.stopPropagation();
+  });
 
 document.body
-	.addEventListener('keydown', function (event) {
-		// Add arrow key navigation through sources
-		switch (event.keyCode) {
-		case 37:
-			console.log('Left key pressed');
-			break;
-		case 38:
-			console.log('Up key pressed');
-			break;
-		case 39:
-			console.log('Right key pressed');
-			break;
-		case 40:
-			console.log('Down key pressed');
-			break;
-		}
-	});
+  .addEventListener('keydown', function(event) {
+    // Add arrow key navigation through sources
+    switch (event.keyCode) {
+      case 37:
+        console.log('Left key pressed');
+        previous();
+        break;
+      case 38:
+        console.log('Up key pressed');
+        previous();
+        break;
+      case 39:
+        console.log('Right key pressed');
+        next();
+        break;
+      case 40:
+        console.log('Down key pressed');
+        next();
+        break;
+    }
+  });
--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # minimal-RTE__ner-training-data
-Minimal customization of Quill.js Rich Text Editor for easy annotation of text snippets for NER model training with spaCy.
+Minimal customization of [Quill.js](https://quilljs.com/) Rich Text Editor for easy annotation of text snippets for NER model training with, f.e. [spaCy](https://spacy.io/).
+
+## Simple RTE for NER Annotation
+
+The interface provides a simple file upload expecting JSON-files containing the following minimal structure:
+
+```JSON
+[
+  { "text": "Lorem ipsum dolor sit."},
+  { "text": "Amet adipiscit verum est."}
+]
+```
+
+The object’s text property is displayed as text inside the RTE. Other properties present in the object’s are not processed and for internal use only, e.g. for referencing or license purposes.
+
+![Image](./data/imgs/ner_editor.png)
+
+The annotated texts can be downloaded as a JSON-file. The interface provides a single download of the currently active text or a bulk download of all texts. E.g.:
+
+```JSON
+[
+    {
+        "text": "Aufgestellt wurde ich, eine Stele, zu Häupten der Angesehenen, Frau Channa, Tochter des Herrn Alexander, die verschieden ist am 14. des Mondes Aw im Jahre 5 Tausend 32 der Zählung. Es sei ihre Seele im Garten Eden, Amen Sela",
+        "entities": [
+            [
+                "50",
+                "61",
+                "TITLE"
+            ],
+            [
+                "68",
+                "74",
+                "PERSON"
+            ],
+            [
+                "88",
+                "93",
+                "TITLE"
+            ],
+            [
+                "94",
+                "103",
+                "PERSON"
+            ],
+            [
+                "202",
+                "213",
+                "PLACE"
+            ]
+        ]
+    },
+    {
+        "text": "Zeuge sei dieser Haufen und Zeugin diese Stele, welche ich errichtet habe zu Häupten der angesehenen jungen Frau, Frau Bela, Tochter des Herrn Natan, die verschied 14. im Tewet des Jahres 5 Tausend und 33 der Zählung. Der Fels bewahre ihre Seele im Garten Eden, Sela",
+        "entities": [
+            [
+                "114",
+                "118",
+                "TITLE"
+            ],
+            [
+                "119",
+                "123",
+                "PERSON"
+            ],
+            [
+                "137",
+                "142",
+                "TITLE"
+            ],
+            [
+                "143",
+                "148",
+                "PERSON"
+            ],
+            [
+                "249",
+                "260",
+                "PLACE"
+            ]
+        ]
+    },
+    {
+        "text": "Diesen Stein, den setzte ich als Stele zu Häupten von Channa, der Jungfrau, Tochter des Herrn Elieser, die verschied Tag 2, 14. des Nissan 43 des sechsten Jahrtausends. Es sei ihre Seele im Garten Eden",
+        "entities": []
+    },
+    {
+        "text": "Dieses Zeichen, welches aufgestellt ward zu Häupten eines Liebenswürdigen, auch Liebenswerten, eingesenkt habe ich es zur Stele. Zu Grabe kam im Alter ein Stattlicher, Jaakow ward sein Name genannt inmitten des verstreuten Volkes, 28. des Adar, Tag 6, verschied Herr Jaakow, Sohn des toragelehrten Herrn Schlomo, 44 des Jahrtausends. Seine Seele sei eingebunden in das Bündel des Lebens, Amen Amen Sela",
+        "entities": []
+    }
+]
+```
+
+## Sample data
+The sample data contained in `data/texts/example.json` is taken from the first four epigraphic records of the catalogue [Frankfurt a.M., Battonstraße](http://www.steinheim-institut.de/cgi-bin/epidat?id=ffb), available on [_EPIDAT ─ Research Platform for Jewish Epigraphy_](http://www.steinheim-institut.de/cgi-bin/epidat?lang=en) built by [Thomas Kollatz](https://orcid.org/0000-0003-1904-1841). The epigraphic records are published under the [Creative Commons Attribution 4.0 International License](https://creativecommons.org/licenses/by/4.0/).
